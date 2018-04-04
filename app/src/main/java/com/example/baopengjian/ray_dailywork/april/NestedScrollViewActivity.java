f3eea2e34cb15a5eda1bb7f5f89bcc8149ebe6e8
baopengjian <baopengjian@2020>
2018-04-04 17:52:24 +0800

    Initial commit

---
+++ app/src/main/java/com/example/baopengjian/ray_dailywork/april/NestedScrollViewActivity.java
@@ -0,0 +1,24 @@
+package com.example.baopengjian.ray_dailywork.april;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.widget.NestedScrollView;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Ray on 2018/4/4 .
+ * NestedScrollView 的用法和ScrollView一样，同时默认可以实现嵌套列表功能
+ *
+ *  requirement: 多层列表
+ *  design：NestedScrollView + RecyclerView
+ */
+
+public class NestedScrollViewActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        NestedScrollView nsv;
+    }
+}
