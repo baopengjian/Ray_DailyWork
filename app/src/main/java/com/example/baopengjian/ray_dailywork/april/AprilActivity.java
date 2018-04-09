480178964e415cc42a28b32cfb38a20e8519d994
baopengjian <baopengjian@2020>
2018-04-09 11:18:53 +0800

    TextView设置文字不同颜色 +部分点击

--- app/src/main/java/com/example/baopengjian/ray_dailywork/april/AprilActivity.java
+++ app/src/main/java/com/example/baopengjian/ray_dailywork/april/AprilActivity.java
@@ -1,20 +1,20 @@
-package com.example.baopengjian.ray_dailywork;
+package com.example.baopengjian.ray_dailywork.april;
 
 import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-import com.example.baopengjian.ray_dailywork.april.AprilActivity;
+import com.example.baopengjian.ray_dailywork.R;
 
-public class MainActivity extends AppCompatActivity {
+public class AprilActivity extends AppCompatActivity {
 
-    static Class[] TARGETS = {AprilActivity.class};
-    static String[] TARGETS_DESC = {"AprilActivity"};
+    static Class[] TARGETS = {NestedScrollViewActivity.class,PartiallyClickableActivity.class};
+    static String[] TARGETS_DESC = {"NestedScrollView嵌套RecyclerView","TextView设置部分颜色+部分点击"};
 
     private Context context;
 
@@ -22,7 +22,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         context = this;
 
         ListView lv = (ListView) findViewById(R.id.lv);
