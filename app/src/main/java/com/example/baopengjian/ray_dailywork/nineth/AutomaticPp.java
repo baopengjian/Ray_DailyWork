1b3ed5e7d99e41b6a4f694de8dc936278dd588fa
bao <1730882047@qq.com>
2019-11-22 18:18:13 +0800

    NumberPicker

--- app/src/main/java/com/example/baopengjian/ray_dailywork/nineth/AutomaticPp.java
+++ app/src/main/java/com/example/baopengjian/ray_dailywork/nineth/AutomaticPp.java
@@ -2,14 +2,18 @@ package com.example.baopengjian.ray_dailywork.nineth;
 
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.ColorDrawable;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.DatePicker;
 import android.widget.NumberPicker;
 import android.widget.PopupWindow;
+import android.widget.Toast;
 
 import com.example.baopengjian.ray_dailywork.R;
+import com.example.baopengjian.ray_dailywork.util.UtilsDensity;
 
 
 /**
@@ -18,58 +22,76 @@ import com.example.baopengjian.ray_dailywork.R;
 public class AutomaticPp {
 
     private PopupWindow popupWindow;
+    private TextNumberPicker tp1, tp2;
 
     public static final String[] TAB1 = {"每月", "每周", "每双周"};
     public static final String[] TAB_WEEK = {"周一", "周二", "周三", "周四", "周五"};
+    public static final int MONTH_MAX = 28;
 
-    public AutomaticPp(Context context) {
+    public AutomaticPp(final Context context) {
         View pop = View.inflate(context, R.layout.pp_financial_auto, null);
         popupWindow = new PopupWindow(pop, ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
         popupWindow.setTouchable(true);
         popupWindow.setFocusable(true);
         popupWindow.setOutsideTouchable(true);
         popupWindow.setBackgroundDrawable(new BitmapDrawable(context.getResources(), (Bitmap) null));
-        //  popupWindow.setAnimationStyle(R.style.shareMenuShow);
 
-        TextNumberPicker tp1 = pop.findViewById(R.id.tp1);
-        tp1.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
-            @Override
-            public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
-
-            }
-        });
+        tp1 = pop.findViewById(R.id.tp1);
         tp1.setDisplayedValues(TAB1);
         tp1.setMaxValue(TAB1.length - 1);
-        tp1.setMinValue(0);
-        tp1.setValue(0);
         tp1.setFormatter(new NumberPicker.Formatter() {
             @Override
             public String format(int i) {
                 return TAB1[i];
             }
         });
-        tp1.setDescendantFocusability(DatePicker.FOCUS_BLOCK_DESCENDANTS);
-        tp1.setWrapSelectorWheel(false);
+        initPicker(tp1, 0);
+        tp2 = pop.findViewById(R.id.tp2);
+        tp2.setMaxValue(MONTH_MAX);
+        initPicker(tp2, 1);
 
-        TextNumberPicker tp2 = pop.findViewById(R.id.tp2);
-        tp2.setDisplayedValues(TAB_WEEK);
-        tp2.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+
+        tp1.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
             @Override
             public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
+                if (newVal == 0) {
+                    tp2.setMinValue(1);
+                    tp2.setMaxValue(28);
+                    tp2.setDisplayedValues(null);
+                    tp2.setValue(1);
+                } else {
+                    tp2.setMinValue(0);
+                    tp2.setMaxValue(TAB_WEEK.length - 1);
+                    tp2.setDisplayedValues(TAB_WEEK);
+                    if (oldVal == 0) {
+                        tp2.setValue(0);
+                    }
+                }
+            }
+        });
 
-            }
-        });
-        tp2.setMaxValue(TAB_WEEK.length - 1);
-        tp2.setMinValue(0);
-        tp2.setValue(0);
-        tp2.setFormatter(new NumberPicker.Formatter() {
+        pop.findViewById(R.id.tv_cancel).setOnClickListener(new View.OnClickListener() {
             @Override
-            public String format(int i) {
-                return String.valueOf(i);
+            public void onClick(View v) {
+                popupWindow.dismiss();
             }
         });
-        tp2.setDescendantFocusability(DatePicker.FOCUS_BLOCK_DESCENDANTS);
-        tp2.setWrapSelectorWheel(false);
+        pop.findViewById(R.id.tv_submit).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context, "tp1 =" + tp1.getValue() + "  tp2 =" + tp2.getValue(), Toast.LENGTH_LONG).show();
+                popupWindow.dismiss();
+            }
+        });
+    }
+
+    private void initPicker(TextNumberPicker picker, int min) {
+        picker.setMinValue(min);
+        picker.setValue(min);
+        picker.setDescendantFocusability(DatePicker.FOCUS_BLOCK_DESCENDANTS);
+        picker.setWrapSelectorWheel(false);
+        TextNumberPicker.setDividerColor(picker, new ColorDrawable(Color.parseColor("#ADB0A7")));
+        TextNumberPicker.setDividerHeight(picker, UtilsDensity.dip2px(0.5f));
     }
 
     public void showAtLocation(View view, int bottom, int x, int y) {
@@ -85,4 +107,6 @@ public class AutomaticPp {
     public boolean isShowing() {
         return popupWindow.isShowing();
     }
+
+
 }
--- app/src/main/java/com/example/baopengjian/ray_dailywork/nineth/TextNumberPicker.java
+++ app/src/main/java/com/example/baopengjian/ray_dailywork/nineth/TextNumberPicker.java
@@ -1,11 +1,16 @@
 package com.example.baopengjian.ray_dailywork.nineth;
 
 import android.content.Context;
+import android.graphics.drawable.ColorDrawable;
 import android.util.AttributeSet;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.NumberPicker;
+
+import com.example.baopengjian.ray_dailywork.util.UtilsDensity;
+
+import java.lang.reflect.Field;
 
 /**
  * Created by Ray on 2019-11-21.
@@ -32,7 +37,36 @@ public class TextNumberPicker extends NumberPicker {
 
     private void updateView(View child) {
         if (child instanceof EditText) {
-            ((EditText)child).setTextSize(24);
+            ((EditText) child).setTextSize(24);
+        }
+    }
+
+    public static void setDividerColor(NumberPicker picker, ColorDrawable color) {
+        Field field = null;
+        try {
+            field = NumberPicker.class.getDeclaredField("mSelectionDivider");
+            if (field != null) {
+                field.setAccessible(true);
+                field.set(picker, color);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void setDividerHeight(NumberPicker picker, int h) {
+        Field[] fields = NumberPicker.class.getDeclaredFields();
+        for (Field field : fields) {
+            if (field.getName().equals("mSelectionDividerHeight")) {
+                field.setAccessible(true);
+                try {
+                    field.set(picker, h);
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                }
+
+                break;
+            }
         }
     }
 }
